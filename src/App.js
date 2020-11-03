7c9c909243916d70a042ec510b175367a09517f6
mustapha chatbi <chatbimdev@gmail.com>
2020-11-03 14:38:43 +0100

    disclaimer modal added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React , {useState} from "react";
 import Home from "./scenes/Home";
 import "./reset.css";
 import "./App.css";
+import DisclaimerModal from './components/DisclaimerModal/DisclaimerModal';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import MainMenu from "./components/Navigations/MainMenu/MainMenu";
+import styled from "styled-components";
+
+
+
 
 function App() {
+
+  const [isModalOpen, setModalOpen] = useState(true);
+
+  const toggleModal = () => setModalOpen(!isModalOpen);
+
+  const StyledCloseButton = styled.button`
+      padding:8px 15px;
+      background-color:#d9534f;
+      color:white;
+      border:none;
+      border-radius:5px;
+      outline:none;
+
+      :hover{
+      background-color:#c9302c;
+      }
+
+      cursor:pointer;
+
+`
+
   return (
     <Router>
       {/* Navigation Component*/}
@@ -18,8 +44,13 @@ function App() {
           <Home/>
         </Route>
       </Switch>
+      <DisclaimerModal isOpen={isModalOpen}>
+      <StyledCloseButton  onClick={toggleModal}>X close</StyledCloseButton>
+
+      </DisclaimerModal>
     </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/DisclaimerModal/DisclaimerModal.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+// 'modal-root' is a sibling to 'app-root'
+const modalRoot = document.getElementById("disclaimer");
+
+export default function DisclaimerModal({ isOpen, children }) {
+  // element to which the modal will be rendered
+  const el = document.createElement("div");
+
+  useEffect(() => {
+    // append to root when the children of Modal are mounted
+    modalRoot.appendChild(el);
+
+    // do a cleanup
+    return () => {
+      modalRoot.removeChild(el);
+    };
+  }, [el]);
+
+  return (
+    isOpen &&
+    createPortal(
+      // child element
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: "100%",
+          width: "100%",
+        //   padding: "100px",
+          backgroundColor: "rgba(0,0,0,0.6)",
+          zIndex:99999999,
+        }}
+      >
+        <p
+          style={{
+            width: "50%",
+            background: "white",
+            padding: "50px",
+            textAlign: "center",
+            position:"relative",
+            fontSize:"20px",
+            top:"50%",
+            left:"50%",
+            transform:"translate(-50%,-50%)"
+          }}
+        >
+            <h1>DISCLAIMER : THIS IS FOR LEARNING PURPOSE ONLY !  </h1>
+            <p >P.S : not trying to overcome instagram ! &#128540; &#128514; </p>
+            <div style={{marginTop:"1rem"}}>
+
+                    {children}
+            </div>
+        </p>
+      </div>,
+      // target container
+      el
+    )
+  );
+}
