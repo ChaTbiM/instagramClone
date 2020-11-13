8d1ebbd70039593c17dcd67adaf8f7fe51d6f9be
mustapha chatbi <chatbimdev@gmail.com>
2020-11-13 09:52:16 +0100

    added env base url variable

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 
 // global config for axios
-axios.defaults.baseURL = "http://localhost:3000/api"; //CHANGE LATER FOR PRODUCTION
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axios.defaults.headers.get["Accept"] = "application/json"; // default header for all get request
 axios.defaults.headers.post["Accept"] = "application/json"; // default header for all POST request
 
