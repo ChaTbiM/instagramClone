24bc0133a482eb3e756cc586093861f111afbd37
mustapha chatbi <chatbimdev@gmail.com>
2020-11-13 09:45:16 +0100

    change endpoints and added home endpoint '/ => hello world'

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5000;
 const posts = require('./factory')
 
 app.get('/',(req,res)=>{
+    res.send("Hello WOrld")
+})
+
+app.get('/posts',(req,res)=>{
     res.status(200).json(posts);
 })
 
